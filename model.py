83d32d29296960be54b0e2d536936b4915f20b80
Your Name <Jose M centeno>
2017-03-20 02:57:58 -0700

    trying LeNet

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import csv
 import numpy as np
 import tensorflow as tf
 import sklearn
+import random
 
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
@@ -26,6 +27,7 @@ with open("udacity_data/driving_log.csv") as udacity_log:
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
@@ -36,21 +38,28 @@ def generator(samples, batch_size=32):
             images = []
             angles = []
             for batch_sample in batch_samples:
+                flip = random.choice([True, False])
                 filename = batch_sample[0].split('/')[-1]
                 name = "udacity_data/IMG/" + filename
                 center_image = cv2.imread(name)
                 center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
+                if not flip:
+                    images.append(center_image)
+                    angles.append(center_angle)
+                else:
+                    flip_image = cv2.flip(center_image, flipCode=1)
+                    images.append(flip_image)
+                    angles.append(-center_angle)
 
+                
             # trim image to only see section with road
             X_train = np.array(images)
             y_train = np.array(angles)
             yield shuffle(X_train, y_train)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=32)
-validation_generator = generator(validation_samples, batch_size=32)
+train_generator = generator(train_samples, batch_size=128)
+validation_generator = generator(validation_samples, batch_size=128)
 
 
 sample_gen = generator(validation_samples, batch_size=1)
@@ -65,7 +74,18 @@ model = Sequential()
 model.add(Cropping2D(cropping=((50,20), (0,0)),
                      input_shape=sample_instance[0].shape[1:]))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Convolution2D(16, 5, 5))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.6))
+model.add(Convolution2D(32, 10, 10))
+model.add(MaxPooling2D((2, 2)))
+model.add(Dropout(0.6))
+model.add(Activation('relu'))
 model.add(Flatten())
+model.add(Dense(240))
+model.add(Activation('relu'))
+model.add(Dense(84))
+model.add(Activation('relu'))
 model.add(Dense(1))
  
 model.compile(loss = 'mse', optimizer= 'adam')
@@ -77,11 +97,11 @@ model.compile(loss = 'mse', optimizer= 'adam')
 
 
 model.fit_generator(train_generator, 
-                    samples_per_epoch = len(train_samples),
+                    samples_per_epoch = 2*len(train_samples),
                     validation_data = validation_generator,
-                    nb_val_samples = len(validation_samples),
-                    nb_epoch = 3,
-                    verbose = 2)
+                    nb_val_samples = 2*len(validation_samples),
+                    nb_epoch = 20,
+                    verbose = 1)
 
 
 print("saving model")
