ddf2027d29db8f825efa94d2aa4bca0d8ad75d4a
Your Name <Jose M centeno>
2017-03-20 01:44:43 -0700

    Changed the mdoel definition based on the generator class

--- model.py
+++ model.py
@@ -1,68 +1,88 @@
-#!/home/jcenteno/miniconda3/envs/carnd-term1/bin/python
+# #!/home/jcenteno/miniconda3/envs/carnd-term1/bin/python
 import cv2
 import csv
 import numpy as np
 import tensorflow as tf
+import sklearn
 
-#tf.python.control_flow_ops = tf
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+# #tf.python.control_flow_ops = tf
 
 # Initial Setup for Keras
 from keras.models import Sequential
 from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
-from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
-lines = []
 
+samples = []
 with open("udacity_data/driving_log.csv") as udacity_log:
     reader = csv.reader(udacity_log)
     header = next(reader)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
+
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for batch_sample in batch_samples:
+                filename = batch_sample[0].split('/')[-1]
+                name = "udacity_data/IMG/" + filename
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield shuffle(X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
 
 
-def get_next_data_point():
-    for line in lines:
-        source_path = line[0]
-        filename = source_path.split("/")[-1]
-        image_path = "udacity_data/IMG/" + filename
-        image = cv2.imread(image_path)
-        measured_angle = float(line[3])
-        yield image, measured_angle
+sample_gen = generator(validation_samples, batch_size=1)
+sample_instance = next(sample_gen)
 
-#Build list
-images = []
-measured_angles = []
-index = 0
-for image, measured_angle in get_next_data_point():
-    images.append(image)
-    measured_angles.append(measured_angle)
-
-    flip_image = cv2.flip(image, flipCode=1)
-    images.append(flip_image)
-    measured_angles.append(-measured_angle)
-
-    # if index == 100:
+print(sample_instance[0].shape)
 
 
-X_train = np.array(images)
-y_train = np.array(measured_angles)
-
-print(X_train.shape, y_train.shape)
-
+# Preprocess incoming data, centered around zero with small standard deviation
 model = Sequential()
-
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=X_train.shape[1:]))
+ 
+model.add(Cropping2D(cropping=((50,20), (0,0)),
+                     input_shape=sample_instance[0].shape[1:]))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 model.add(Flatten())
 model.add(Dense(1))
-
+ 
 model.compile(loss = 'mse', optimizer= 'adam')
-model.fit(X_train,
-          y_train,
-          validation_split = 0.2,
-          shuffle = True,
-          nb_epoch=2)
+# model.fit(X_train,
+#           y_train,
+#           validation_split = 0.2,
+#           shuffle = True,
+#           nb_epoch=2)
+
+
+model.fit_generator(train_generator, 
+                    samples_per_epoch = len(train_samples),
+                    validation_data = validation_generator,
+                    nb_val_samples = len(validation_samples),
+                    nb_epoch = 3,
+                    verbose = 2)
+
 
 print("saving model")
 model.save('model.h5')
