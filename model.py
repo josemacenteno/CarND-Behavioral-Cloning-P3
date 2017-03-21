0af9c4d705aee7586337573bf20c06dcc0fb7ea1
Your Name <Jose M centeno>
2017-03-20 23:08:20 -0700

    data augmentation with side cameras

--- model.py
+++ model.py
@@ -19,6 +19,8 @@ from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
 from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
+DEV = False
+keep_prob = 0.4
 
 samples = []
 with open("udacity_data/driving_log.csv") as udacity_log:
@@ -31,6 +33,8 @@ train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 
 def generator(samples, batch_size=32):
+    CENTER, LEFT, RIGHT = (0,1,2)
+    ORIENTATION_OPTIONS = [RIGHT, LEFT, CENTER]
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -41,16 +45,24 @@ def generator(samples, batch_size=32):
             angles = []
             for batch_sample in batch_samples:
                 flip = random.choice([True, False])
-                filename = batch_sample[0].split('/')[-1]
+                image_orientation = random.choice(ORIENTATION_OPTIONS)
+                if image_orientation == CENTER:
+                    angle_correction = 0.0
+                elif image_orientation == LEFT:
+                    angle_correction = 0.15
+                else:
+                    angle_correction = -0.15
+
+                filename = batch_sample[image_orientation].split('/')[-1]
                 name = "udacity_data/IMG/" + filename
-                center_image = cv2.imread(name)
-                noise_factor = random.uniform(0.96, 1.04)
-                center_angle = noise_factor * float(batch_sample[3])
+                train_image = cv2.imread(name)
+                noise_factor = random.uniform(1.00, 1.05)
+                center_angle = noise_factor * (float(batch_sample[3]) + angle_correction)
                 if not flip:
-                    images.append(center_image)
+                    images.append(train_image)
                     angles.append(center_angle)
                 else:
-                    flip_image = cv2.flip(center_image, flipCode=1)
+                    flip_image = cv2.flip(train_image, flipCode=1)
                     images.append(flip_image)
                     angles.append(-center_angle)
 
@@ -77,28 +89,31 @@ model = Sequential()
 model.add(Cropping2D(cropping=((40,10), (0,0)),
                      input_shape=sample_instance[0].shape[1:]))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Convolution2D(24, 5, 5))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.6))
-model.add(Convolution2D(36, 5, 5))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.6))
-model.add(Convolution2D(48, 5, 5))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.6))
-model.add(Convolution2D(64, 3, 3))
-model.add(MaxPooling2D((2, 2)))
-model.add(Dropout(0.6))
-model.add(Flatten())
-model.add(Dense(100))
-model.add(Activation('relu'))
-model.add(Dropout(0.6))
-model.add(Dense(50))
-model.add(Activation('relu'))
-model.add(Dropout(0.6))
-model.add(Dense(10))
-model.add(Activation('relu'))
-model.add(Dropout(0.6))
+if DEV:
+    model.add(Flatten())
+else:
+    model.add(Convolution2D(24, 5, 5))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(keep_prob))
+    model.add(Convolution2D(36, 5, 5))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(keep_prob))
+    model.add(Convolution2D(48, 5, 5))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(keep_prob))
+    model.add(Convolution2D(64, 3, 3))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(keep_prob))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    model.add(Dropout(keep_prob))
+    model.add(Dense(50))
+    model.add(Activation('relu'))
+    model.add(Dropout(keep_prob))
+    model.add(Dense(10))
+    model.add(Activation('relu'))
+    model.add(Dropout(keep_prob))
 model.add(Dense(1))
  
 model.compile(loss = 'mse', optimizer= 'adam')
