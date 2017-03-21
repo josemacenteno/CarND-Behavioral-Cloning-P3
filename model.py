afdda576ca34293b12d5593acb01858f08cdfb8e
Your Name <Jose M centeno>
2017-03-21 04:05:37 -0700

    data augmentation techniques added. Still drives too straight

--- model.py
+++ model.py
@@ -20,7 +20,9 @@ from keras.layers.convolutional import Convolution2D, Cropping2D
 from keras.layers.pooling import MaxPooling2D
 
 DEV = False
-keep_prob = 0.4
+MAX_SHIFT = 30
+keep_prob = 0.3
+MAX_NOISE = 0.05
 
 samples = []
 with open("udacity_data/driving_log.csv") as udacity_log:
@@ -32,9 +34,10 @@ with open("udacity_data/driving_log.csv") as udacity_log:
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 
+
 def generator(samples, batch_size=32):
     CENTER, LEFT, RIGHT = (0,1,2)
-    ORIENTATION_OPTIONS = [RIGHT, LEFT, CENTER]
+    ORIENTATION_OPTIONS = [CENTER, LEFT, RIGHT]
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
         shuffle(samples)
@@ -53,10 +56,17 @@ def generator(samples, batch_size=32):
                 else:
                     angle_correction = -0.15
 
+
                 filename = batch_sample[image_orientation].split('/')[-1]
                 name = "udacity_data/IMG/" + filename
                 train_image = cv2.imread(name)
-                noise_factor = random.uniform(1.00, 1.05)
+                noise_factor = random.uniform(1-MAX_NOISE, 1 + MAX_NOISE)
+                if float(batch_sample[3]) < 0.001:
+                    shift_factor = random.randint(-MAX_SHIFT,MAX_SHIFT)
+                    rows,cols = train_image.shape[0:2]
+                    M = np.float32([[1,0,shift_factor],[0,1,0]])
+                    train_image = cv2.warpAffine(train_image,M,(cols,rows))
+                    angle_correction += shift_factor/1000
                 center_angle = noise_factor * (float(batch_sample[3]) + angle_correction)
                 if not flip:
                     images.append(train_image)
@@ -86,46 +96,41 @@ print(sample_instance[0].shape)
 # Preprocess incoming data, centered around zero with small standard deviation
 model = Sequential()
  
-model.add(Cropping2D(cropping=((40,10), (0,0)),
+model.add(Cropping2D(cropping=((40,10), (MAX_SHIFT,MAX_SHIFT)),
                      input_shape=sample_instance[0].shape[1:]))
-model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+model.add(Lambda(lambda x: (x / 128.0) - 1.0))
 if DEV:
     model.add(Flatten())
 else:
     model.add(Convolution2D(24, 5, 5))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(keep_prob))
-    model.add(Convolution2D(36, 5, 5))
-    model.add(MaxPooling2D((2, 2)))
-    model.add(Dropout(keep_prob))
     model.add(Convolution2D(48, 5, 5))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(keep_prob))
-    model.add(Convolution2D(64, 3, 3))
+    model.add(Convolution2D(64, 5, 5))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Dropout(keep_prob))
+    model.add(Convolution2D(128, 3, 3))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(keep_prob))
     model.add(Flatten())
-    model.add(Dense(100))
+    model.add(Dense(150))
     model.add(Activation('relu'))
     model.add(Dropout(keep_prob))
-    model.add(Dense(50))
+    model.add(Dense(75))
     model.add(Activation('relu'))
     model.add(Dropout(keep_prob))
-    model.add(Dense(10))
+    model.add(Dense(20))
     model.add(Activation('relu'))
     model.add(Dropout(keep_prob))
 model.add(Dense(1))
  
 model.compile(loss = 'mse', optimizer= 'adam')
-# model.fit(X_train,
-#           y_train,
-#           validation_split = 0.2,
-#           shuffle = True,
-#           nb_epoch=2)
 
 
 history_object = model.fit_generator(train_generator, 
-                    samples_per_epoch = len(train_samples),
+                    samples_per_epoch = len(train_samples),  
                     validation_data = validation_generator,
                     nb_val_samples = len(validation_samples),
                     nb_epoch = 5,
@@ -151,5 +156,3 @@ plt.show()
 
 
 print("Done")
-
-
